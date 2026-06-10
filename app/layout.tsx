a64e728a322d1bbef8ecbc41dfea1d6c4a18e10f
Salsabila2609 <harjantosalsabila@gmail.com>
2026-06-10 13:49:22 +0700

    add PWA

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css'
 export const metadata: Metadata = {
   title: 'Nota Reimburse',
   description: 'Sistem reimbursement nota driver',
+  manifest: '/manifest.json',
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
@@ -13,6 +14,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;600&display=swap" rel="stylesheet" />
+        <meta name="theme-color" content="#000000" />
       </head>
       <body>{children}</body>
     </html>
