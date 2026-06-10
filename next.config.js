fa9d4712386630bdd56c6186731cec68204aacfc
Salsabila2609 <128382995+Salsabila2609@users.noreply.github.com>
2026-06-10 14:04:42 +0700

    Add turbopack

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const withPWA = require('next-pwa')({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  turbopack: {},
   experimental: {
     serverActions: {
       bodySizeLimit: '15mb'
